662afeaeb856364d5ef5ef9a983822dfaa307784
JoelRdb <joelrandriambao08@gmail.com>
2025-05-16 12:33:43 +0300

    Creation de la class BrandContextSeed dans le Dossier Data pour alimenter une collection depuis le fichier brands.json pour les données initiales.

---
+++ Ecommerce/Services/Catalog/Catalog.Infrastructure/Data/BrandContextSeed.cs
@@ -0,0 +1,35 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data
+{
+    public static class BrandContextSeed
+    {
+        //Ajouter les donées du fichier brands.json dans une collection pour 
+        public static void SeedData(IMongoCollection<ProductBrand> brandCollection)
+        {
+            //Any(): Vérifie s'il y a au moins un élément dans le résultat de Find
+            //Le prédicat b => true: on ne filtre rien (commme SELECT *)
+            bool checkBrands = brandCollection.Find(b => true).Any();
+            string path = Path.Combine("Data", "SeedData", "brands.json");
+            if (!checkBrands)
+            {
+                var brandsData = File.ReadAllText(path);
+                var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                if (brands != null)
+                {
+                    foreach (var item in brands)
+                    {
+                        brandCollection.InsertOneAsync(item);
+                    }
+                }
+            }
+        }
+    }
+}
