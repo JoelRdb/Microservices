cd412cfd7aac95917d716c180dd2a0802f3b7c58
JoelRdb <joelrandriambao08@gmail.com>
2025-05-16 11:17:34 +0300

    Creation de la classe ProductBrand et ProductType pour le type des propriétés dans la class Product.

---
+++ Ecommerce/Services/Catalog/Catalog.Core/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Core.Entities
+{
+    public class BaseEntity
+    {
+        [BsonId] //Indique que c'est une clé primaire
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)] //Stock/lit l'Id comme un ObjectId BSON dans MongoDB
+        public string Id { get; set; }
+    }
+}
