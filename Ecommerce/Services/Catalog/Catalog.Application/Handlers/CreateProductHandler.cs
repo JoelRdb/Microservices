0e8bd5895b18dbb1f16e9f3c5ee1115c3c7e8ebb
JoelRdb <joelrandriambao08@gmail.com>
2025-05-21 13:30:09 +0300

    Création de Create Product Command Handler.

---
+++ Ecommerce/Services/Catalog/Catalog.Application/Handlers/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Application.Commands;
+using Catalog.Application.Mappers;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers
+{
+    public class CreateProductHandler(IProductRepository repositoryProduct) : IRequestHandler<CreateProductCommand, ProductResponse>
+    {
+        private readonly IProductRepository _repositoryProduct;
+        public async Task<ProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var productEntity = ProductMapper.Mapper.Map<Product>(request);
+            if (productEntity is null)
+            {
+                throw new ApplicationException("Il y a un problème de mappage lors de la création d'un nouveau produit");
+            }
+            var newProduct = await _repositoryProduct.CreatProduct(productEntity);
+            var productResponse = ProductMapper.Mapper.Map<ProductResponse>(newProduct);
+            return productResponse;
+        }
+    }
+}
