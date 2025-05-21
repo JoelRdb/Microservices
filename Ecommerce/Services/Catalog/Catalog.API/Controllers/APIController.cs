dbaf6f76b06db7aa3f22e3739e9f9fbc99ab1fed
JoelRdb <joelrandriambao08@gmail.com>
2025-05-21 14:34:23 +0300

    Création du controlleur de base: APIController dans la couche présentation (API).

---
+++ Ecommerce/Services/Catalog/Catalog.API/Controllers/APIController.cs
@@ -0,0 +1,12 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiVersion("V1")]
+    [Route("api/v{version:ApiVersion}/[controller]")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+    }
+}
