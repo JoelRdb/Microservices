510f16dbf1f5d7294d3e122cedba19881c76fa80
JoelRdb <joelrandriambao08@gmail.com>
2025-05-21 14:02:25 +0300

    Création de Delete Product By Id Handler.

---
+++ Ecommerce/Services/Catalog/Catalog.Application/Handlers/DeleteProductHandler.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Application.Commands;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class DeleteProductHandler(IProductRepository repository) : IRequestHandler<DeleteProductByIdCommand, bool>
+    {
+        private readonly IProductRepository _repository = repository;
+      
+        public async Task<bool> Handle(DeleteProductByIdCommand request, CancellationToken cancellationToken)
+        {
+           return await _repository.DeleteProduct(request.Id);
+            
+        }
+    }
+}
