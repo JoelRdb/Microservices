88beeb12d57543661da8583b75acad4b29a49ca9
JoelRdb <joelrandriambao08@gmail.com>
2025-05-21 11:35:50 +0300

    Implementation de Get Product By Name with MediatR.

---
+++ Ecommerce/Services/Catalog/Catalog.Application/Handlers/GetProductByNameHandler.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetProductByNameHandler(IProductRepository repository) : IRequestHandler<GetProductByNameQuery, IList<ProductResponse>>
+    {
+        private readonly IProductRepository _repository = repository;
+
+        public async Task<IList<ProductResponse>> Handle(GetProductByNameQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _repository.GetProductsByName(request.Name);
+            var productsResponseList = ProductMapper.Mapper.Map<IList<ProductResponse>>(products);
+            return productsResponseList;
+        }
+    }
+}
