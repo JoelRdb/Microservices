52c152764000bae2d01fc5e66991a36dc0705374
JoelRdb <joelrandriambao08@gmail.com>
2025-06-05 19:10:59 +0300

    Creating Get Basket By User Name Query Handler.

---
+++ Ecommerce/Services/Basket/Basket.Application/Hadlers/GetBasketByUserNameHandler.cs
@@ -0,0 +1,19 @@
+﻿using Basket.Application.Mappers;
+using Basket.Application.Queries;
+using Basket.Application.Responses;
+using Basket.Core.Repositories;
+using MediatR;
+
+namespace Basket.Application.Hadlers
+{
+    public class GetBasketByUserNameHandler(IBasketRepository repository) : IRequestHandler<GetBasketByUserNameQuery, ShoppingCartResponse>
+    {
+        private readonly IBasketRepository _basketRepository = repository;
+        public async Task<ShoppingCartResponse> Handle(GetBasketByUserNameQuery request, CancellationToken cancellationToken)
+        {
+            var shoppingCart = await _basketRepository.GetBasket(request.UserName);
+            var shoppingCartResponse = BasketMapper.Mapper.Map<ShoppingCartResponse>(shoppingCart);
+            return shoppingCartResponse;
+        }
+    }
+}
