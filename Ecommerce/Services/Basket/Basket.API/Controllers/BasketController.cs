b64ae0e6bf6a56bf4b14ca7286a1b1b7f13a7705
JoelRdb <joelrandriambao08@gmail.com>
2025-06-07 12:44:33 +0300

    Creating Basket Controller : CreateBasket, DeleteBasket

--- Ecommerce/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ Ecommerce/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,7 +1,7 @@
-﻿using Basket.Application.Queries;
+﻿using Basket.Application.Commands;
+using Basket.Application.Queries;
 using Basket.Application.Responses;
 using MediatR;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
@@ -23,6 +23,25 @@ namespace Basket.API.Controllers
         {
             var query = new GetBasketByUserNameQuery(userName);
             var basket = await _mediator.Send(query);
+            return Ok(basket);
+        }
+
+
+        [HttpPost("CreateBasket")]
+        [ProducesResponseType(typeof(ShoppingCartResponse), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCartResponse>> UpdateBasket([FromBody] CreateShoppingCartCommand createShoppingCartCommand)
+        {
+            var basket = await _mediator.Send(createShoppingCartCommand);
+            return Ok(basket);
+        }
+
+        [HttpDelete]
+        [Route("[action]/{userName}", Name = "DeleteBasketByName")]
+        [ProducesResponseType((int) HttpStatusCode.OK)]
+        public async Task<ActionResult> DeleteBasket(string userName)
+        {
+            var cmd = new DeleteBasketByUserNameCommand(userName);
+            var basket = await _mediator.Send(cmd);
             return Ok(basket);
         }
     }
