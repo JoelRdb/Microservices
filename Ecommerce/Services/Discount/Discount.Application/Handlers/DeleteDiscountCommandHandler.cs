e2a76ca394a31a03b9bfe0317b107c5a8cef6736
JoelRdb <joelrandriambao08@gmail.com>
2025-06-08 17:09:40 +0300

    Creating Delete Command Handler

---
+++ Ecommerce/Services/Discount/Discount.Application/Handlers/DeleteDiscountCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Discount.Application.Commands;
+using Discount.Core.Repositories;
+using MediatR;
+
+namespace Discount.Application.Handlers
+{
+    public class DeleteDiscountCommandHandler(IDiscountRepository discountRepository) : IRequestHandler<DeleteDiscountCommand, bool>
+    {
+        private readonly IDiscountRepository _discountRepository = discountRepository;
+        
+        public Task<bool> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var deleted = _discountRepository.DeleteDiscount(request.ProductName);
+            return deleted;
+        }
+    }
+}
