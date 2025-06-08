9ea0b3c2357a231d195dad182c6835b55b8baa98
JoelRdb <joelrandriambao08@gmail.com>
2025-06-08 16:58:12 +0300

    Create Coupon Command Handler

---
+++ Ecommerce/Services/Discount/Discount.Application/Handlers/CreateDiscountCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Discount.Application.Commands;
+using Discount.Core.Entities;
+using Discount.Core.Repositories;
+using Discount.Grpc.Proto;
+using MediatR;
+
+namespace Discount.Application.Handlers
+{
+    public class CreateDiscountCommandHandler(IDiscountRepository discountRepository, IMapper mapper) : IRequestHandler<CreateDiscountCommand, CouponModel>
+    {
+        private readonly IDiscountRepository _discountRepository = discountRepository;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<CouponModel> Handle(CreateDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var coupon = _mapper.Map<Coupon>(request);
+            await _discountRepository.CreateDiscount(coupon);
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+            return couponModel;
+        }
+    }
+}
