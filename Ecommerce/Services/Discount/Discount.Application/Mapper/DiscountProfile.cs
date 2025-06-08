a4dd604b898f770d9770b44125c1dbb68dc44733
JoelRdb <joelrandriambao08@gmail.com>
2025-06-08 16:39:10 +0300

    Creating Discount Profile in Mapper

---
+++ Ecommerce/Services/Discount/Discount.Application/Mapper/DiscountProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Discount.Core.Entities;
+using Discount.Grpc.Proto;
+
+namespace Discount.Application.Mapper
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            //Change property of discount.poto like this : (Build Action : Protobuf compiler ; gRPC Stub Classes : Server only).
+            // CouponModel created automatiacally after buil Discount.Application by gRPC (discount.proto) 
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
