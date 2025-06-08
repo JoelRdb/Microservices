b0d322bf5daf827a5b39b9e87494a223a129197d
JoelRdb <joelrandriambao08@gmail.com>
2025-06-08 17:05:14 +0300

    Update Coupon Command Handler

---
+++ Ecommerce/Services/Discount/Discount.Application/Commands/UpdateDiscountCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Discount.Core.Entities;
+using Discount.Core.Repositories;
+using Discount.Grpc.Proto;
+using MediatR;
+
+namespace Discount.Application.Commands
+{
+    public class UpdateDiscountCommandHandler(IDiscountRepository discountRepository, IMapper mapper) : IRequestHandler<UpdateDiscountCommand, CouponModel>
+    {
+        private readonly IDiscountRepository _discountRepository = discountRepository;
+        public readonly IMapper _mapper = mapper;
+
+        public async Task<CouponModel> Handle(UpdateDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var coupon = _mapper.Map<Coupon>(request);
+            await _discountRepository.UpdateDiscount(coupon);
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+            return couponModel;
+        }
+    }
+}
