8a869c3837e307b5fd5f642080b5d7c90cc62eab
JoelRdb <joelrandriambao08@gmail.com>
2025-06-08 16:48:56 +0300

    Get Discount Query Handler

---
+++ Ecommerce/Services/Discount/Discount.Application/Handlers/GetDiscountQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Discount.Application.Mapper;
+using Discount.Application.Queries;
+using Discount.Core.Repositories;
+using Discount.Grpc.Proto;
+using Grpc.Core;
+using MediatR;
+
+namespace Discount.Application.Handlers
+{
+    class GetDiscountQueryHandler(IDiscountRepository discountRepository) : IRequestHandler<GetDiscountQuery, CouponModel>
+    {
+        private readonly IDiscountRepository _discountRepository = discountRepository;
+
+        public async Task<CouponModel> Handle(GetDiscountQuery request, CancellationToken cancellationToken)
+        {
+            var coupon = await _discountRepository.GetDiscount(request.ProductName);
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount for Product Name = {request.ProductName} not found"));
+            }
+            var couponModel = new CouponModel
+            {
+                Id = coupon.Id,
+                ProductName = coupon.ProductName,
+                Amount = coupon.Amount,
+                Description = coupon.Description
+            };
+            return couponModel;
+        }
+    }
+}
