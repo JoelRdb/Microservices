b2acd5af3b9dbb47c1eb469a3369a5f234d73bc1
JoelRdb <joelrandriambao08@gmail.com>
2025-06-10 18:12:38 +0300

    Creating Entities

---
+++ Ecommerce/Services/Ordering/Ordering.Core/Common/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Core.Common
+{
+    public abstract class EntityBase
+    {
+        // Protected set us made to use in the derived classes
+        public int Id { get; protected set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? LastModifyBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
