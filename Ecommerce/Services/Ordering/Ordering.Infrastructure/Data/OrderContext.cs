e063fb41fbbb7d6627c2f0c275a28850105d187c
JoelRdb <joelrandriambao08@gmail.com>
2025-06-10 18:48:46 +0300

    Creating Order Context Class

---
+++ Ecommerce/Services/Ordering/Ordering.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Common;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext>options):base(options)
+        {
+            
+        }
+
+        DbSet<Order> Orderes { get; set; }
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "joël"; //TODO: Replace with auth server
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifyBy = "joël"; //TODO: Replace with auth server
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+}
