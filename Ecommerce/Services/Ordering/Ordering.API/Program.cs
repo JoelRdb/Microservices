7ffe27142466dfd2e8e0db59e9606a26e60ec028
JoelRdb <joelrandriambao08@gmail.com>
2025-06-11 16:24:10 +0300

    Calling Migrate Database from Program file

--- Ecommerce/Services/Ordering/Ordering.API/Program.cs
+++ Ecommerce/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,7 @@
 using Asp.Versioning;
+using Ordering.API.Extensions;
 using Ordering.Application.Extensions;
+using Ordering.Infrastructure.Data;
 using Ordering.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +26,13 @@ builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.M
 
 
 var app = builder.Build();
+
+//Apply Db Migration
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    var logger = services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed.SeedAsync(context, logger).Wait();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
