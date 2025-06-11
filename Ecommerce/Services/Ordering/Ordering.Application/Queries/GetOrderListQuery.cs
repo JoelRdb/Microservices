b31fd109c440be8b21c66b03a489dcc34875385a
JoelRdb <joelrandriambao08@gmail.com>
2025-06-11 11:57:15 +0300

    Creating Get Order List Query and Order List Query Handler

---
+++ Ecommerce/Services/Ordering/Ordering.Application/Queries/GetOrderListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Ordering.Application.Responses;
+
+namespace Ordering.Application.Queries
+{
+    public class GetOrderListQuery : IRequest<List<OrderResponse>>
+    {
+        public string _userName { get; set; }
+
+        public GetOrderListQuery(string userName)
+        {
+            _userName = userName;
+        }
+    }
+}
