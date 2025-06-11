0c4e753cd13a8570e40712a3937c06bd94ff8323
JoelRdb <joelrandriambao08@gmail.com>
2025-06-11 11:46:08 +0300

    Creating Order Mapping Profile

---
+++ Ecommerce/Services/Ordering/Ordering.Application/Mappers/OrderMappingProfle.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+
+namespace Ordering.Application.Mappers
+{
+    public class OrderMappingProfle : Profile
+    {
+        public OrderMappingProfle()
+        {
+            CreateMap<Order, OrderResponse>().ReverseMap();
+        }
+    }
+}
