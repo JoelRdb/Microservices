22d6d81e07f8a44665c7188ec382cecf62d24fa2
JoelRdb <joelrandriambao08@gmail.com>
2025-06-11 15:51:17 +0300

    Creating Program.cs File

--- Ecommerce/Services/Ordering/Ordering.API/Program.cs
+++ Ecommerce/Services/Ordering/Ordering.API/Program.cs
@@ -1,17 +1,36 @@
+using Asp.Versioning;
+using Ordering.Application.Extensions;
+using Ordering.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddApiVersioning(options =>
+{
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+});
+
+// Application Services
+builder.Services.AddApplicationServices();
+
+// Infra services
+builder.Services.AddInfraServices(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Ordering.API", Version = "v1" }); });
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
