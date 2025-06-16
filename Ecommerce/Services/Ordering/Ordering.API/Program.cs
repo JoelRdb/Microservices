ca48bf097020d91afdb1b76ec50d85c82a4ec35c
JoelRdb <joelrandriambao08@gmail.com>
2025-06-16 19:00:11 +0300

    Wiring up Mass Transit in  Ordering API

--- Ecommerce/Services/Ordering/Ordering.API/Program.cs
+++ Ecommerce/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,7 @@
 using Asp.Versioning;
+using EventBus.Messages.E_vents.Common;
+using MassTransit;
+using Ordering.API.EventBusConsumer;
 using Ordering.API.Extensions;
 using Ordering.Application.Extensions;
 using Ordering.Infrastructure.Data;
@@ -21,9 +24,28 @@ builder.Services.AddApplicationServices();
 // Infra services
 builder.Services.AddInfraServices(builder.Configuration);
 
+//Consumer class
+builder.Services.AddScoped<BasketOrderingConsumer>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Ordering.API", Version = "v1" }); });
 
+//Mass Transit
+builder.Services.AddMassTransit(config =>
+{
+    // Mark this as consumer
+    config.AddConsumer<BasketOrderingConsumer>();
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+        // provide the queue name with consumer settings
+        cfg.ReceiveEndpoint(EventBusConstant.BasketCheckoutQueue, c =>
+        {
+            c.ConfigureConsumer<BasketOrderingConsumer>(ctx);
+        });
+    });
+});
+builder.Services.AddMassTransitHostedService();
 
 var app = builder.Build();
 
