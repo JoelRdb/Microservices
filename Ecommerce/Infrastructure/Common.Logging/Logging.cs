048fb476eaa0399178bf7c3052515f8e9e35fd75
JoelRdb <joelrandriambao08@gmail.com>
2025-06-19 12:06:46 +0300

    Add comment in Logging.cs

--- Ecommerce/Infrastructure/Common.Logging/Logging.cs
+++ Ecommerce/Infrastructure/Common.Logging/Logging.cs
@@ -6,19 +6,28 @@ namespace Common.Logging
 {
     public static class Logging
     {
+        // Déclare une fonction réutilisable qui configure Serilog.
+        /* HostBuilderContext → fournit l’environnement (Development, Production, etc.)
+           LoggerConfiguration → l’objet à configurer
+        */
         public static Action<HostBuilderContext, LoggerConfiguration> cfg =>
             (context, loggerConfiguration) =>
             {
                 var env = context.HostingEnvironment;
-                loggerConfiguration.MinimumLevel.Information()
-                .Enrich.FromLogContext()
-                .Enrich.WithProperty("ApplicationName", env.ApplicationName)
-                .Enrich.WithProperty("EnvironmentName", env.EnvironmentName)
-                .Enrich.WithExceptionDetails()
-                .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
+                loggerConfiguration.MinimumLevel.Information() //Le niveau de log minimum global est Information (ignore Debug et Trace par défaut).
+                .Enrich.FromLogContext() //Ajoute automatiquement des propriétés du contexte de log, comme RequestId, UserId, etc., quand elles sont disponibles.
+                .Enrich.WithProperty("ApplicationName", env.ApplicationName) // Ajoute un propriété personnalisé à chaque log :le nom de l'application
+                .Enrich.WithProperty("EnvironmentName", env.EnvironmentName) //// Ajoute un propriété personnalisé à chaque log :le nom de l'application
+                .Enrich.WithExceptionDetails()  //Utilise le package Serilog.Exceptions pour enrichir les logs d’erreur avec tous les détails des exceptions.
+                .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)   // Les logs ASP.NET Core système ne seront logués qu’à partir de Warning pour éviter de polluer la console avec du bruit.
                 .MinimumLevel.Override("Microsoft.Hosting.Lifetime", Serilog.Events.LogEventLevel.Warning)
-                .WriteTo.Console();
+                .WriteTo.Console(); //Envoie les logs formatés dans la console (très utile en développement et dans Docker).
 
+                /*
+                    Quand l’app tourne en environnement Development :
+                    Les microservices Catalog, Basket, Discount, Ordering ont un niveau minimum Debug
+                    Pratique pour avoir plus de détails pendant le dev, sans changer le niveau global.
+                */
                 if (context.HostingEnvironment.IsDevelopment())
                 {
                     loggerConfiguration.MinimumLevel.Override("Catalog", Serilog.Events.LogEventLevel.Debug);
