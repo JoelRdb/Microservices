ec23c5c73214330fe512aafd476b1c9e6495879d
JoelRdb <joelrandriambao08@gmail.com>
2025-06-23 18:22:57 +0300

    Creating Basket Controller V2 Version

---
+++ Ecommerce/Services/Basket/Basket.API/Controllers/V2/BasketController.cs
@@ -0,0 +1,55 @@
+﻿using Asp.Versioning;
+using Basket.Application.Commands;
+using Basket.Application.Mappers;
+using Basket.Application.Queries;
+using Basket.Core.Entities;
+using EventBus.Messages.Common;
+using EventBus.Messages.E_vents;
+using MassTransit;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers.V2
+{
+    [ApiVersion("2")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        public IMediator _mediator { get; }
+        public ILogger<Controllers.BasketController> _logger { get; }
+
+        public IPublishEndpoint _publishEndpoint;
+
+        public BasketController(IMediator mediator, IPublishEndpoint publishEndpoint, ILogger<Controllers.BasketController> logger)
+        {
+            _mediator = mediator;
+            _publishEndpoint = publishEndpoint;
+            _logger = logger;
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckoutV2 basketCheckout)
+        {
+            var query = new GetBasketByUserNameQuery(basketCheckout.UserName);
+            var basket = await _mediator.Send(query);
+            if (basket == null)
+            {
+                return BadRequest();
+            }
+            var eventMsg = BasketMapper.Mapper.Map<BasketCheckoutEventV2>(basketCheckout);
+            eventMsg.TotalPrice = basket.TotalPrice;
+            await _publishEndpoint.Publish(eventMsg);
+
+            _logger.LogInformation($"Basket Published for {basket.UserName} with V2 endpoint");
+
+            var deleteCmd = new DeleteBasketByUserNameCommand(basket.UserName);
+            await _mediator.Send(deleteCmd);
+            return Accepted();
+        }
+    }
+}
