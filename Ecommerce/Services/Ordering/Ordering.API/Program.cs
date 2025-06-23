e8125aa8e3dcec0c9a4a0c6c46c0c64e14a204d0
JoelRdb <joelrandriambao08@gmail.com>
2025-06-23 21:53:16 +0300

    Wiring  Up Program  File Changes

--- Ecommerce/Services/Ordering/Ordering.API/Program.cs
+++ Ecommerce/Services/Ordering/Ordering.API/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddInfraServices(builder.Configuration);
 
 //Consumer class
 builder.Services.AddScoped<BasketOrderingConsumer>();
+builder.Services.AddScoped<BasketOrderingConsumerV2>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Ordering.API", Version = "v1" }); });
@@ -42,6 +43,7 @@ builder.Services.AddMassTransit(config =>
 {
     // Mark this as consumer
     config.AddConsumer<BasketOrderingConsumer>();
+    config.AddConsumer<BasketOrderingConsumerV2>();
     config.UsingRabbitMq((ctx, cfg) =>
     {
         cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
@@ -49,6 +51,11 @@ builder.Services.AddMassTransit(config =>
         cfg.ReceiveEndpoint(EventBusConstant.BasketCheckoutQueue, c =>
         {
             c.ConfigureConsumer<BasketOrderingConsumer>(ctx);
+        });
+        //V2 Version 
+        cfg.ReceiveEndpoint(EventBusConstant.BasketCheckoutQueueV2, c =>
+        {
+            c.ConfigureConsumer<BasketOrderingConsumerV2>(ctx);
         });
     });
 });
