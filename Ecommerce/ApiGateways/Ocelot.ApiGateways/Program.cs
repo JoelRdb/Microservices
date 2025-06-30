6efd4ee3df1cecd05b64cb9dee96383506491bb2
JoelRdb <joelrandriambao08@gmail.com>
2025-06-30 14:46:46 +0300

    Project creation & development of Program.cs

---
+++ Ecommerce/ApiGateways/Ocelot.ApiGateways/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot()
+    .AddCacheManager(o => o.WithDictionaryHandle());
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Hello Ocelot");
+    });
+});
+
+await app.UseOcelot();
