93c462bc30a91e354bfed30364a60f7cb193c213
JoelRdb <joelrandriambao08@gmail.com>
2025-07-11 18:11:07 +0300

    Implementing Http Client

--- Ecommerce/client/src/app/app.component.ts
+++ Ecommerce/client/src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [
+    NavbarComponent,
+    HttpClientModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+
+
+export class AppComponent implements OnInit{
+  title = 'VATSY MDG';
+
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get('https://id-local.eshopping.com:44344/Catalog/GetProductByBrandName/Adidas').subscribe({
+      next:response => console.log(response),
+      error: error => console.log(error),
+      complete:() => {
+        console.log('Catalog API call completed');
+      }
+    });
+  }
 }
