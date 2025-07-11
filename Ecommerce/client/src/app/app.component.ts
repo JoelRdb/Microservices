5df87cde658002cc30d6925d24bce9f369a0a69a
JoelRdb <joelrandriambao08@gmail.com>
2025-07-11 21:00:46 +0300

    Fecthing Products, print data on Angular

--- Ecommerce/client/src/app/app.component.ts
+++ Ecommerce/client/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     NavbarComponent,
     HttpClientModule,
+    CommonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -16,13 +19,17 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 export class AppComponent implements OnInit{
   title = 'VATSY MDG';
+  products: any[] = []
 
   constructor(private http: HttpClient) {
-
   }
+
   ngOnInit(): void {
     this.http.get('https://localhost:8010/Catalog/GetProductByBrandName/Adidas').subscribe({
-      next:response => console.log(response),
+      next:(response:any) => {
+        this.products = response,
+        console.log(response)
+      },
       error: error => console.log(error),
       complete:() => {
         console.log('Catalog API call completed');
