a09e97b84b9df63cdff745ae0fb508672420dc64
JoelRdb <joelrandriambao08@gmail.com>
2025-07-12 12:42:08 +0300

    Modifying Store Page html and create product items in Store

---
+++ Ecommerce/client/src/app/store/product-items/product-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-items',
+  imports: [],
+  templateUrl: './product-items.component.html',
+  styleUrl: './product-items.component.scss'
+})
+export class ProductItemsComponent {
+
+}
