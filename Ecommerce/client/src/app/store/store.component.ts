4f92f2ef5c5bd95fe6f5570c321005138ec8c5d7
JoelRdb <joelrandriambao08@gmail.com>
2025-07-14 18:08:15 +0300

    Fechting Brands and Types

--- Ecommerce/client/src/app/store/store.component.ts
+++ Ecommerce/client/src/app/store/store.component.ts
@@ -3,12 +3,16 @@ import { StoreService } from './store.service';
 import { IProduct } from '../shared/models/product';
 import { CommonModule } from '@angular/common';
 import { ProductItemsComponent } from './product-items/product-items.component';
+import { types } from 'util';
+import { IBrand } from '../shared/models/brand';
+import { IType } from '../shared/models/type';
 
 @Component({
   selector: 'app-store',
   imports: [
     CommonModule,
     ProductItemsComponent,
+
   ],
   templateUrl: './store.component.html',
   styleUrl: './store.component.scss'
@@ -18,14 +22,33 @@ export class StoreComponent implements OnInit {
    *
    */
   products: IProduct[] = [] ;
+  brands: IBrand[] = [];
+  types: IType[] = [];
+
   constructor(private storeService: StoreService) {}
 
   ngOnInit(): void {
-    this.storeService.getProducts().subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-    });
+  
   }
   
-  
+  getProducts(){
+    this.storeService.getProducts().subscribe({
+        next: response => this.products = response.data,
+        error: error => console.log(error),
+    });
+  }
+
+  getBrands(){
+    this.storeService.getBrands().subscribe({
+        next: response => this.brands = response,
+        error: error => console.log(error),
+    });
+  }  
+  getTypes(){
+    this.storeService.getTypes().subscribe({
+        next: response => this.types = response,
+        error: error => console.log(error),
+    });
+  }
+
 }
--- Ecommerce/client/src/app/store/store.service.ts
+++ Ecommerce/client/src/app/store/store.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPagination } from '../shared/models/pagination';
 import { IProduct } from '../shared/models/product';
+import { IBrand } from '../shared/models/brand';
+import { IType } from '../shared/models/type';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,13 @@ export class StoreService {
 
     getProducts() {
       return this.httpClient.get<IPagination<IProduct[]>>(this.baseUrl + 'Catalog/GetAllProducts');
+    }
+
+    getBrands() {
+      return this.httpClient.get<IBrand[]>(this.baseUrl + 'Catalog/GetAllBrands');
+    }
+
+    getTypes() {
+      return this.httpClient.get<IType[]>(this.baseUrl + 'Catalog/GetAllTypes');
     }
 }
