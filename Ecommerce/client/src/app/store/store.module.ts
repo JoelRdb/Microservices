a0188de3d725c3d4da85495a5e0f120e8dad8a02
JoelRdb <joelrandriambao08@gmail.com>
2025-07-17 15:13:24 +0300

    Adding Pagination Module

--- Ecommerce/client/src/app/store/store.module.ts
+++ Ecommerce/client/src/app/store/store.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap/pagination'; // Importing PaginationModule for pagination functionality
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    PaginationModule.forRoot()  // Importing PaginationModule for pagination functionality
   ]
 })
 export class StoreModule { }
