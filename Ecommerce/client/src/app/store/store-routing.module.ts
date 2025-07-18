9897e503d20f89f8523cfd6008a470eeb6121f72
JoelRdb <joelrandriambao08@gmail.com>
2025-07-18 15:52:25 +0300

    Implementing Lazy Loading
    Le lazy loading signifie que certains modules ne sont chargés que lorsqu’ils sont nécessaires, par exemple au moment où l’utilisateur accède à une route spécifique.
    
    👉 Ça permet d’accélérer le chargement initial de l’app (utile si tu as une grosse app ou plusieurs modules indépendants).

--- Ecommerce/client/src/app/store/store-routing.module.ts
+++ Ecommerce/client/src/app/store/store-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
+import { StoreComponent } from './store.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-
+const routes: Routes = [
+  // Define your routes here
+  {path:'', component:StoreComponent},
+  {path:'store', loadChildren: () => import('./store.module').then(m => m.StoreModule)},
+  {path: '**', redirectTo: '', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class StoreRoutingModule { }
--- Ecommerce/client/src/app/store/store.module.ts
+++ Ecommerce/client/src/app/store/store.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination'; // Importing PaginationModule for pagination functionality
 import { RouterModule } from '@angular/router';
+import { StoreRoutingModule } from './store-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()  // Importing PaginationModule for pagination functionality    
+    PaginationModule.forRoot(),  // Importing PaginationModule for pagination functionality
+    StoreRoutingModule
   ]
 })
 export class StoreModule { }
