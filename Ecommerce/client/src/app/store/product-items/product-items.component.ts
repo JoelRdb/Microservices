5fbf01828dd9618f5f59657f665d1d3455122f9d
JoelRdb <joelrandriambao08@gmail.com>
2025-07-18 14:45:20 +0300

    Creating Router Links in Navbar
    Rendre le menu Home et Store fonctionnel pour la navigation

--- Ecommerce/client/src/app/store/product-items/product-items.component.ts
+++ Ecommerce/client/src/app/store/product-items/product-items.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { IProduct } from '../../shared/models/product';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-items',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './product-items.component.html',
   styleUrl: './product-items.component.scss'
 })
--- Ecommerce/client/src/app/store/store.module.ts
+++ Ecommerce/client/src/app/store/store.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination'; // Importing PaginationModule for pagination functionality
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()  // Importing PaginationModule for pagination functionality
+    PaginationModule.forRoot()  // Importing PaginationModule for pagination functionality    
   ]
 })
 export class StoreModule { }
