3e26876819f0cc7e799f514e2e01e9c5042d6776
JoelRdb <joelrandriambao08@gmail.com>
2025-07-24 17:47:27 +0300

    Setting Breadcrumb dynamically
    Header now displays breadcrumbs and a dynamic title based on the current route, using xng-breadcrumb. Unused static title and welcome message were removed from app.component.html for a cleaner layout.

--- Ecommerce/client/src/app/core/header/header.component.ts
+++ Ecommerce/client/src/app/core/header/header.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { BreadcrumbComponent} from 'xng-breadcrumb';
+import { BreadcrumbComponent, BreadcrumbService} from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-header',
-  imports: [BreadcrumbComponent],
+  imports: [
+    BreadcrumbComponent, 
+    CommonModule
+],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  constructor(public bcService: BreadcrumbService) {
+    // Set the breadcrumb for the header component
+
+    
+  }
 }
