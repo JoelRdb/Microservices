b76ef9536e38a5bbddf95f119471531a3d7aa005
JoelRdb <joelrandriambao08@gmail.com>
2025-07-25 12:54:51 +0300

    Add home page carousel and marketing sections
    Introduced a carousel with six hero images and three marketing featurettes to the home page. Added related images, updated HTML structure, applied new SCSS styles for images and layout, and imported CarouselModule in the component. Also added global styles for breadcrumb elements.

--- Ecommerce/client/src/app/home/home.component.ts
+++ Ecommerce/client/src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {CarouselModule} from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
